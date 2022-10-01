f3b09c66cb008c4af7c40876225c6ae8822f6eca
ronnielopez <39448096+ronnielopez@users.noreply.github.com>
2022-09-30 20:59:23 -0600

    :+1:

---
+++ Parcial 3/Controllers/AccessController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial_3.Models;
+
+namespace Parcial_3.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string user, string password)
+        {
+            try
+            {
+                using (Parcial3Entities db = new Parcial3Entities())
+                {
+                    //select * from --- >Esto es LINQ
+                    // select * from MIS_DATOS as d 
+                    var lst = from d in db.users
+                              where d.nombre == user && d.pwd == password
+                              select d;  //select * from MIS_DATOS
+                    if (lst.Count() > 0)
+                    {
+                        user oUser = lst.First();
+
+                        Session["User"] = oUser;
+
+                        return Content("1");
+
+                    }
+                    else
+                    {
+                        return Content("Usuario sin Acceso ");
+                    }
+
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return Content("Error de aplicativo" + ex.Message);
+            }
+
+        }
+
+
+    }
+}
---
+++ Parcial 3/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Parcial_3.Models.player;
+
+namespace Parcial_3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        string Baseurl = "http://localhost:3000/leagues";
+        public async Task<ActionResult> Index()
+        {
+            Root EmpInfo = new Root();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage Res = await client.GetAsync("");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    EmpInfo = JsonConvert.DeserializeObject<Root>(EmpResponse);
+
+                }
+                return View(EmpInfo);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
